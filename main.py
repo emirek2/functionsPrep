5c9346ea61121768cb7890d9c499b704020e5615
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-10-25 17:29:51 +0000

    functions

---
+++ main.py
@@ -0,0 +1,19 @@
+from add import add_fruit
+from divide import divide_fruit
+from multiply import multiply_fruit
+
+apples=int(input('How many apples:  '))
+oranges=int(input('how many oranges:  '))
+
+
+#function call for adding apple and oranges
+print(add_fruit(apples, oranges))
+
+
+#Function for dividing and oranges
+print(divide_fruit(apples, oranges))
+
+
+
+#function for multiplying apples and oranges
+print(multiply_fruit(apples, oranges))
